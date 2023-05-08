c87570c63ff6c895baa9417c2d626277163575ce
doleg17 <132674101+doleg17@users.noreply.github.com>
2023-05-08 19:14:00 +0200

    virial

--- cell.py
+++ cell.py
@@ -14,13 +14,13 @@ class Cell:
 		The three lattice vectors (in Angstrom).
 	energy : float
 		The total energy (in eV).
-	stress : list[list[float]]
-		The stress tensor (in kbar).
+	virial : list[list[float]]
+		The virial tensor (in kbar).
 	atoms : list[Atom]
 		The contained atoms.
 	"""
 
-	def __init__(self, lattice : List[List[float]], energy: float, stress: List[List[float]], atoms: List[Atom]) -> None:
+	def __init__(self, lattice : List[List[float]], energy: float, virial: List[List[float]], atoms: List[Atom]) -> None:
 		"""
 		Parameters
 		----------
@@ -28,13 +28,13 @@ class Cell:
 			The three lattice vectors (in Angstrom)
 		energy : float
 			The total energy (in eV)
-		stress : list[list[float]]
-			The stress tensor (in kbar)
+		virial : list[list[float]]
+			The virial tensor (in kbar)
 		atoms : list[Atom]
 			The atoms in the cell
 		"""
 
 		self.lattice = lattice
 		self.energy = energy
-		self.stress = stress
+		self.virial = virial
 		self.atoms = atoms

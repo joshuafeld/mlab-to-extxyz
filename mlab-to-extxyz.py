d75853710329d88cff54e53a54f38775bf299317
Joshua Feld <mail@joshuafeld.com>
2023-05-10 10:38:26 +0200

    :art: Convert indentation from tabs to spaces
    Signed-off-by: Joshua Feld <mail@joshuafeld.com>

--- mlab-to-extxyz.py
+++ mlab-to-extxyz.py
@@ -7,17 +7,17 @@ from parser import parse_mlab
 from writer import write_extxyz
 
 def main():
-	input_path = 'ML_AB'
-	if len(sys.argv) >= 2:
-		input_path = os.path.abspath(sys.argv[1])
-		if not os.path.exists(input_path):
-			print('the input file you provided does not exist or the path to it is wrong')
-			return
+    input_path = 'ML_AB'
+    if len(sys.argv) >= 2:
+        input_path = os.path.abspath(sys.argv[1])
+        if not os.path.exists(input_path):
+            print('the input file you provided does not exist or the path to it is wrong')
+            return
 
-	output_path = 'output.extxyz'
-	if len(sys.argv) >= 3:
-		output_path = os.path.abspath(sys.argv[2])
+    output_path = 'output.extxyz'
+    if len(sys.argv) >= 3:
+        output_path = os.path.abspath(sys.argv[2])
 
-	write_extxyz(parse_mlab(input_path), output_path)
+    write_extxyz(parse_mlab(input_path), output_path)
 
 main()
--- writer.py
+++ writer.py
@@ -3,4 +3,4 @@ from ase.io import write
 from cell import Cell
 
 def write_extxyz(cells: list[Cell], path: str):
-	write(path, images=[cell.to_ase() for cell in cells], format='extxyz')
+    write(path, images=[cell.to_ase() for cell in cells], format='extxyz')

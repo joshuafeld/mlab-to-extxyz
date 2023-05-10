6510826d3aa155fab120934d86e23c9aaef39eb8
Joshua Feld <mail@joshuafeld.com>
2023-05-10 10:21:01 +0200

    :art: Separate ML AB parser and extended XYZ writer
    Signed-off-by: Joshua Feld <mail@joshuafeld.com>

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+from ase import Atoms
+from ase.io import write
+
 from atom import Atom
 from cell import Cell
 
@@ -36,43 +39,44 @@ def get_vec3_list(raw: list[str], name: str, ledger: str) -> list[list[float]]:
     floats = get_float_list(raw, name, ledger)
     return [floats[i:i + 3] for i in range(0, len(floats), 3)]
 
-# Read the input file into a list of lines.
-with open('ML_AB') as file:
-    ml_ab = [' '.join(line.split()) for line in file]
+def parse_mlab(path: str) -> list[Cell]:
+    # Read the input file into a list of lines.
+    with open(path) as file:
+        ml_ab = [' '.join(line.split()) for line in file]
+    
+    # Get the number of cells in the ML_AB file.
+    cell_count: int = get_int(ml_ab, 'The number of configurations')
+    print(str(cell_count))
 
-# Get the number of cells in the ML_AB file.
-cell_count: int = get_int(ml_ab, 'The number of configurations')
+    # Create a list of cells.
+    cells: list[Cell] = []
+    for cell_index in range(1, cell_count + 1):
+        if cell_index % 10 == 0:
+            print(f'Parsing cell {cell_index}/{cell_count}')
 
-# Create a list of cells.
-cells: list[Cell]
-for cell_index in range(1, cell_count + 1):
-    if cell_index % 10 == 0:
-        print(f'Parsing cell {cell_index}/{cell_count}')
+        # Extract the relevant lines for the current cell from the input file.
+        start: int = ml_ab.index(f'Configuration num. {cell_index}')
+        raw: list[str] = ml_ab[start:ml_ab.index('XY YZ ZX', start) + 3]
 
-    # Extract the relevant lines for the current cell from the input file.
-    start: int = ml_ab.index(f'Configuration num. {cell_index}')
-    raw: list[str] = ml_ab[start:ml_ab.index('XY YZ ZX', start) + 3]
+        # Read the cell data for the current cell.
+        lattice: list[list[float]] = get_vec3_list(raw, 'Primitive lattice vectors (ang.)', EQUAL_LEDGER)
+        energy: float = get_float(raw, 'Total energy (eV)')
+        stress: list[list[float]] = [get_vec3(raw, 'XX YY ZZ'), get_vec3(raw, 'XY YZ ZX')]
 
-    # Read the cell data for the current cell.
-    lattice: list[list[float]] = get_vec3_list(raw, 'Primitive lattice vectors (ang.)', EQUAL_LEDGER)
-    energy: float = get_float(raw, 'Total energy (eV)')
-    stress: list[list[float]] = [get_vec3(raw, 'XX YY ZZ'), get_vec3(raw, 'XY YZ ZX')]
+        # Read the atom data for the current cell.
+        species_counts: list[str] = get_list(raw, 'Atom types and atom numbers', EQUAL_LEDGER)
+        species: list[str] = species_counts[0::2]
+        counts: list[int] = list(map(int, species_counts[1::2]))
+        pos: list[list[float]] = get_vec3_list(raw, 'Atomic positions (ang.)', EQUAL_LEDGER)
+        forces: list[list[float]] = get_vec3_list(raw, 'Forces (eV ang.^-1)', EQUAL_LEDGER)
 
-    # Read the atom data for the current cell.
-    species_counts: list[str] = get_list(raw, 'Atom types and atom numbers', EQUAL_LEDGER)
-    species: list[str] = species_counts[0::2]
-    counts: list[int] = list(map(int, species_counts[1::2]))
-    pos: list[list[float]] = get_vec3_list(raw, 'Atomic positions (ang.)', EQUAL_LEDGER)
-    forces: list[list[float]] = get_vec3_list(raw, 'Forces (eV ang.^-1)', EQUAL_LEDGER)
+        # Create a list of the atoms in the current cell.
+        atoms: list[Atom] = []
+        for j, (s, c) in enumerate(zip(species, counts)):
+            for k in range(c):
+                l: int = sum(counts[:j]) + k
+                atoms.append(Atom(s, pos[l], forces[l]))
 
-    # Create a list of the atoms in the current cell.
-    atoms = []
-    for j, (s, c) in enumerate(zip(species, counts)):
-        for k in range(c):
-            l: int = sum(counts[:j]) + k
-            atoms.append(Atom(s, pos[l], forces[l]))
-
-    # Create and append the current cell to the list of cells.
-    cells.append(Cell(lattice, energy, stress, atoms))
-
-# TODO: Do something with the created cells list.
+        # Create and append the current cell to the list of cells.
+        cells.append(Cell(lattice, energy, stress, atoms))
+    return cells
---
+++ writer.py
@@ -0,0 +1,6 @@
+from ase.io import write
+
+from cell import Cell
+
+def write_extxyz(cells: list[Cell], path: str):
+	write(path, images=[cell.to_ase() for cell in cells], format='extxyz')
